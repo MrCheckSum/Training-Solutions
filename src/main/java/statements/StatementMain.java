f7ebbf0e01a4265dbad14f1cd454b7d9880cd079
Szlovik Péter <peter.szlovik@outlook.com>
2020-11-08 19:55:28 +0100

    Kifejezések feladat

---
+++ src/main/java/statements/StatementMain.java
@@ -0,0 +1,11 @@
+package statements;
+
+public class StatementMain {
+
+    int x = 5 + 6;
+    int y = 11-x;
+    boolean b = if (x > y) {true};
+    boolean c = if(b == true && z > 5) {true};
+    int z;
+    z = z + 1;
+}
