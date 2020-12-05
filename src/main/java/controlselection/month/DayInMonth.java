93fea08630241b03637678d8177f558d2993b9ba
Szlovik Péter <peter.szlovik@outlook.com>
2020-12-05 16:16:38 +0100

    Hónap napjainak visszaadása feladat

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,37 @@
+package controlselection.month;
+
+import java.util.Locale;
+
+public class DayInMonth {
+
+    public int nrOfDays(int year, String month) {
+        switch (month.toLowerCase()) {
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                return 31;
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                return 30;
+            case "február":
+                if ((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            default:
+                throw new IllegalArgumentException("Nem jó hónap! (" + month + ")");
+        }
+    }
+
+    public static void main(String[] args) {
+        DayInMonth dayInMonth = new DayInMonth();
+        System.out.println(dayInMonth.nrOfDays(2019,"február"));
+    }
+}
