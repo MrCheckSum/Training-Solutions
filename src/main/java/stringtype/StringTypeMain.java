8c97d1b1934acd8c3cf4b6cf54073f31dccf9c27
Szlovik Péter <peter.szlovik@outlook.com>
2021-02-15 08:57:05 +0100

    StringType feladat

--- src/main/java/stringtype/StringTypeMain.java
+++ src/main/java/stringtype/StringTypeMain.java
@@ -25,6 +25,7 @@ public class StringTypeMain {
         System.out.println(testText.substring(0, 1) + ", " + testText.substring(2, 3));
         System.out.println(testText.substring(0, 2));
 
+
     }
 
 }
